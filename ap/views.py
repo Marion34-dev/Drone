99c1e2b5bbf14b8e74049fdf80997b8ba6dcc849
Marion Jung <marionjung34@hotmail.com>
2023-11-24 11:43:37 +0000

    Streaming coordinates from Arduino

--- ap/views.py
+++ ap/views.py
@@ -1,57 +1,11 @@
 import asyncio
 import telnetlib
-import json
-from django.http import JsonResponse, StreamingHttpResponse
+from django.http import StreamingHttpResponse
 from django.views import View
-from django.shortcuts import render
-
-
 
 HOST = "192.168.1.149"
-""" 
-class ReceiveDataView(View):
-    async def get_data(self):
-        loop = asyncio.get_event_loop()
 
-        try:
-            tn = telnetlib.Telnet(HOST)
-            database_result = await loop.run_in_executor(None, tn.read_until, b"\n", 1.1)
-            return {"data": database_result.decode("utf-8")}
-        
-        except Exception as e:
-            return {"error": str(e)}
-
-    async def get(self, request):
-        data = await self.get_data()
-        return JsonResponse(data)
-
- """
-""" 
-class ReceiveDataView(View):
-    async def get_data(self):
-        try:
-            tn = telnetlib.Telnet(HOST)
-            database_result = await asyncio.to_thread(tn.read_until, b"\n", 1.1)
-            tn.close()
-            return {"data": database_result.decode("utf-8")}
-        except Exception as e:
-            return {"error": str(e)}
-
-    async def continuous_execution(self):
-        while True:
-            data = await self.get_data()
-            print("Continuous Execution:", data)
-            await asyncio.sleep(1)  # Adjust the sleep duration as needed
-
-    async def get(self, request):
-        # Start the continuous execution in the background
-        asyncio.create_task(self.continuous_execution())
-
-    
-        data = await self.get_data()
-        return JsonResponse(data) """
-
-"""CURRENTLY STREAMS DATA"""
+"""STREAMING DATA"""
 class ReceiveDataView(View):
     async def get_data(self):
         try:
@@ -62,7 +16,7 @@ class ReceiveDataView(View):
         except Exception as e:
             return {"error": str(e)}
         
-# DISPLAYING OLD VALUES AND NEW VALUES IN BROWSER
+# DISPLAYING OLD VALUES AS WELL AS NEW VALUES IN BROWSER
     """ async def continuous_execution(self):
         while True:
             data = await self.get_data()
@@ -88,14 +42,4 @@ class ReceiveDataView(View):
 
     def get(self, request):
         return StreamingHttpResponse(self.continuous_execution(), content_type="text/event-stream")
-    
-
-class LocationView(View):
-    def get(self, request):
-        lat = request.GET.get('latitude', 0.0)
-        lng = request.GET.get('longitude', 0.0)
-
-        # Return the values as JSON
-        data = {'latitude': lat, 'longitude': lng}
-        return render(request, 'map.html', {'latitude': lat, 'longitude': lng})
     
