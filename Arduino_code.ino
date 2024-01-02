f2fad52a230aed2269df28393d9d2ab2d0954040
Marion <125993993+Marion34-dev@users.noreply.github.com>
2024-01-02 15:14:20 +0000

    Annotations Arduino_code.ino

--- Arduino_code.ino
+++ Arduino_code.ino
@@ -6,6 +6,7 @@
 int keyIndex = 0; // network key Index number (needed only for WEP)
 int status = WL_IDLE_STATUS;
 
+// WiFi server on port 23
 WiFiServer server(23);
 bool alreadyConnected = false;
 
@@ -13,21 +14,25 @@ float GPS_Latitude = 0.0;
 float GPS_Longitude = 0.0;
 float GPS_Altitude = 0.0;
 
+// Buffer to store GPS data
 char GPSBuffer[82];
 byte GPSIndex = 0;
 
 void setup()
 {
+   // Start serial communication with a GPS module
     Serial1.begin(9600);
     pinMode(GPSENABLE, OUTPUT);
     digitalWrite(GPSENABLE, HIGH);
 
+    // Check if the WiFi shield is present
     if (WiFi.status() == WL_NO_SHIELD)
     {
         while (true)
             ;
     }
 
+    // Connect to WiFi network
     while (status != WL_CONNECTED)
     {
         status = WiFi.begin(ssid, pass);
@@ -40,6 +45,7 @@ void setup()
 
 void loop()
 {
+    // Check for a client connection
     WiFiClient client = server.available();
 
     CheckGPS();
@@ -50,12 +56,14 @@ void loop()
     {
         if (!alreadyConnected)
         {
+            // Flush the client buffer if not already connected
             client.flush();
             alreadyConnected = true;
         }
 
         if (client.available() > 0)
         {
+            // Read a character from the client and write it to the server
             char thisChar = client.read();
             server.write(thisChar);
         }
@@ -71,18 +79,22 @@ void printWiFiStatus()
 
 void CheckGPS()
 {
+    // Process incoming GPS data
     int inByte;
     while (Serial1.available() > 0)
     {
+        // Reset the GPS buffer index if the start of a new message is detected
         inByte = Serial1.read();
         if ((inByte == '$') || (GPSIndex >= 81))
         {
             GPSIndex = 0;
         }
+        // Store the incoming character in the GPS buffer (except for carriage return)
         if (inByte != '\r')
         {
             GPSBuffer[GPSIndex++] = inByte;
         }
+        // Trigger ProcessGPSLine when a newline character is received
         if (inByte == '\n')
         {
             ProcessGPSLine();
@@ -93,6 +105,7 @@ void CheckGPS()
 
 void ProcessGPSLine()
 {
+    // Check if the message is a GNGGA command
     if ((GPSBuffer[1] == 'G') && (GPSBuffer[2] == 'N') && (GPSBuffer[3] == 'G') && (GPSBuffer[4] == 'G') && (GPSBuffer[5] == 'A'))
     {
         ProcessGNGGACommand();
@@ -101,6 +114,7 @@ void ProcessGPSLine()
 
 void ProcessGNGGACommand()
 {
+    // Write the GPS data to the server
     server.write(GPSBuffer, GPSIndex);
 
     char *token = strtok(GPSBuffer, ",");
